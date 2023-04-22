55d96bba1dbb98fe767085141ed182aafda2859b
coffeelot <slutastalkamig@gmalil.com>
2023-04-22 11:03:28 +0200

    fix cop count

--- client/client.lua
+++ client/client.lua
@@ -83,7 +83,7 @@ local function checkSkills(diff)
 end
 
 local function checkCops(diff)
-    return Config.Jobs[diff].minimumPolice >= CurrentCops
+    return Config.Jobs[diff].minimumPolice <= CurrentCops
 end
 
 local function getAllContent(object, diff)
@@ -122,7 +122,7 @@ local function canInteract(diff)
         hasToken = exports['cw-tokens']:hasToken(Config.Jobs[diff].token)
     end
 
-    return hasSkills and hasToken
+    return hasSkills and hasToken and hasCops
 end
 
 --- Create bosses
